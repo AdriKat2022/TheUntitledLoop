72afd38590fb6240934be5f4555a308b236a0890
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2025-03-08 21:28:10 +0100

    add reloop systeme

---
+++ Assets/Scripts/PnjDeplacement.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct inputHistory
+{
+    public float time;
+    public float value;
+
+    public inputHistory(float time, float value)
+    {
+        this.time = time;
+        this.value = value;
+    }
+
+    public string ToString()
+    {
+        return "time : " + time + ",value : " + value;
+    }
+}
+
+public class PnjDeplacement : MonoBehaviour
+{
+
+    public Vector3 initialPosition;
+    public bool reproduce = true;
+    public List<inputHistory> history = new List<inputHistory>();
+    private int currentInputId = 0;
+    private Coroutine co;
+
+    private float _currentHorizontalInput;
+    [SerializeField] private float _acceleration = 50f;
+    [SerializeField] private float _deceleration = 50f;
+    private Rigidbody2D _rigidbody;
+
+    [HideInInspector]
+    public int SpeedMultiplier = 100; // Modified by something external
+    [SerializeField] private float _topSpeed = 2f;
+
+    private IEnumerator ReproduceInput()
+    {
+        while (reproduce && currentInputId < history.Count)
+        {
+            _currentHorizontalInput = history[currentInputId].value;
+            yield return new WaitForSeconds(history[currentInputId].time);
+            currentInputId++;
+        }
+        _currentHorizontalInput = 0;
+    }
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        ManageMovement();
+    }
+
+    private void ManageMovement()
+    {
+        if (_currentHorizontalInput != 0)
+        {
+            // Normalize the input if needed
+            float targetVelocity = (SpeedMultiplier / 100f) * _topSpeed * _currentHorizontalInput;
+
+            float acceleration = (targetVelocity - _rigidbody.velocity.x) * _acceleration;
+            _rigidbody.AddForce(acceleration * Vector2.right, ForceMode2D.Force);
+        }
+        else
+        {
+            Vector2 deceleration = -_rigidbody.velocity * _deceleration;
+            _rigidbody.AddForce(deceleration, ForceMode2D.Force);
+            
+        }
+    }
+
+    public void Launch() 
+    { 
+        currentInputId = 0;
+        if(co != null) StopCoroutine(co);
+        co = StartCoroutine(ReproduceInput());
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = initialPosition;
+    }
+}
