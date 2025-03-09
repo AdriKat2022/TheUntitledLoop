63eefe1fc6676cf87a12c9c60db4c8b7cc657722
AdriKat2022 <adrien.schroedel91@gmail.com>
2025-03-09 12:09:35 +0100

    updated character interaction and main menu title

--- Assets/Scripts/Interaction/CharacterInteraction.cs
+++ Assets/Scripts/Interaction/CharacterInteraction.cs
@@ -1,12 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CharacterInteraction : MonoBehaviour
 {
+    [Header("Interaction Settings")]
+    [Tooltip("If true, the first interaction is the one that will be interacted with.\nIf false, it's the last newer one that was added.")]
+    [SerializeField] private bool _useFirstInteraction = true; // If true, the interaction will be done with the first interaction found
+    [Space]
     [SerializeField] private InputActionReference _interactionInputReference;
     [SerializeField] private Animator _interactionHint;
 
-    private Interactable _currentInteractable;
+    private readonly List<Interactable> _interactables = new();
     private bool _isInteracting = false;
 
     #region Input Management
@@ -26,11 +31,25 @@ public class CharacterInteraction : MonoBehaviour
     {
         if (_isInteracting) return;
 
-        if (_currentInteractable != null)
+        if (_interactables != null && _interactables.Count > 1)
         {
-            _isInteracting = true;
-            _interactionHint.SetBool("InInteraction", true);
-            _currentInteractable.Interact(EndInteractionCallback);
+            PerformInteraction();
+        }
+    }
+    #endregion
+
+    private void PerformInteraction()
+    {
+        _isInteracting = true;
+        _interactionHint.SetBool("InInteraction", true);
+
+        if (_useFirstInteraction)
+        {
+            _interactables[0].Interact(EndInteractionCallback);
+        }
+        else
+        {
+            _interactables[^1].Interact(EndInteractionCallback);
         }
     }
 
@@ -39,14 +58,67 @@ public class CharacterInteraction : MonoBehaviour
         _isInteracting = false;
         _interactionHint.SetBool("InInteraction", false);
     }
-    #endregion
 
-    public void SetInteractable(Interactable interactable)
+    public void AddPossibleInteraction(Interactable interactable)
     {
-        _currentInteractable = interactable;
+        if (interactable == null)
+        {
+            Debug.LogError("Tried to add a null interactable.");
+            return;
+        }
+
+        _interactables.Add(interactable);
         if (_interactionHint != null)
         {
-            _interactionHint.SetBool("IsVisible", interactable != null);
+            _interactionHint.SetBool("IsVisible", true);
+        }
+
+        CheckNextInteractable();
+    }
+
+    public void RemovePossibleInteraction(Interactable interactable)
+    {
+        if (interactable == null)
+        {
+            Debug.LogError("Tried to remove a null interactable.");
+            return;
+        }
+
+        // Find the interactable and remove it
+        for (int i = 0; i < _interactables.Count; i++)
+        {
+            if (_interactables[i] == interactable)
+            {
+                _interactables[i].Highlight(false);
+                _interactables.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (_interactionHint != null)
+        {
+            _interactionHint.SetBool("IsVisible", _interactables.Count > 0);
+        }
+
+        CheckNextInteractable();
+    }
+
+    private void CheckNextInteractable()
+    {
+        if (_interactables.Count == 0) return;
+
+        foreach (Interactable interactable in _interactables)
+        {
+            interactable.Highlight(false);
+        }
+
+        if (_useFirstInteraction)
+        {
+            _interactables[0].Highlight(true);
+        }
+        else
+        {
+            _interactables[^1].Highlight(true);
         }
     }
 }
