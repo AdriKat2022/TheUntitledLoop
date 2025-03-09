632738a6368584c48aeb9c7b9db12d43f59a1ec3
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2025-03-09 14:36:24 +0100

    add title for dialogue

--- Assets/Scripts/Dialogues/DialogueManager.cs
+++ Assets/Scripts/Dialogues/DialogueManager.cs
@@ -86,7 +86,7 @@ public class DialogueManager : Singleton<DialogueManager>
 
     private void EndDialogue(Action onDialogueEndCallback)
     {
-        Debug.Log("End of dialogue");
+        //Debug.Log("End of dialogue");
         gameObject.SetActive(false);
         _isInDialogue = false;
 
@@ -102,8 +102,8 @@ public class DialogueManager : Singleton<DialogueManager>
             Debug.Log(title);
             story.SetNextNode(title != null ? title : story.GetStart());
         }
-
-        Debug.Log("Going through story : " + story);
+        
+        //Debug.Log("Going through story : " + story);
         int nextNodesCount = 0;
         StoryNode currentNode;
 
@@ -119,7 +119,9 @@ public class DialogueManager : Singleton<DialogueManager>
             }
             nextNodesCount = currentNode.GetNextNodes().Count;
 
-            _titleText.text = currentNode.GetTitle();
+            if(title != null) _titleText.text = GetFullName(title.Split('_')[ currentNode.HasTag("Reponse") ? 1 : 0]);
+            else _titleText.text = currentNode.GetTitle();
+
             _mainText.text = currentNode.getText();
 
             // If the title text begins with // then hide the title text container
@@ -135,12 +137,12 @@ public class DialogueManager : Singleton<DialogueManager>
                 // Display options to the player
                 yield return DisplayOptions(currentNode);
 
-                Debug.Log("Option count: " + nextNodesCount);
+                //Debug.Log("Option count: " + nextNodesCount);
 
                 // Wait for player to choose an option
                 yield return new WaitUntil(() => _isOptionSelected);
 
-                Debug.Log("Option selected: " + _optionSelected);
+                //Debug.Log("Option selected: " + _optionSelected);
 
                 story.ChooseNextNode(_optionSelected % nextNodesCount);
             }
@@ -167,6 +169,19 @@ public class DialogueManager : Singleton<DialogueManager>
         UpdateVariables(story);
 
         EndDialogue(onDialogueEndCallback);
+    }
+
+    private string GetFullName(string nameShort)
+    {
+        switch (nameShort)
+        {
+            case "Ce": return "Mangaka";
+            case "Ch": return "Chien";
+            case "P": return "Professeur";
+            case "E": return "Enfant";
+        }
+
+        return "// ???";
     }
 
     private void UpdateVariables(Story story)
